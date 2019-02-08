16d4e271b498004811dbee1904e8df2713b4d9db
Mat2dr <dragovic.mathias@gmail.com>
2019-02-08 20:37:31 +0100

    Category complet
    Working:
    -ADD category
    -REMOVE category
    -MODIFY category
    Problem:
    -All the setFlash not showing up

--- admin/category.php
+++ admin/category.php
@@ -2,16 +2,38 @@
 include '../lib/includes.php';
 include '../partials/admin_header.php';
 
+
+
+/**
+SUPPRESSION
+**/
+if(isset($_GET['delete'])){
+    checkCsrf();
+    $id = $db->quote($_GET['delete']);
+    $db->query("DELETE FROM categories WHERE id=$id");
+    setFlash('La catégorie a bien été supprimée');
+    header('location:category.php');
+    die();
+}
+
+/**
+CATEGORIES
+**/
 $select = $db->query('SELECT id, name, slug FROM categories');
 $categories = $select->fetchAll();
 
+
 ?>
 
-<div class="containers">
+<div class="container">
 <h1>Les catégories</h1>
+    
+<div class="container">
+<p><a href="category_edit.php" class="btn btn-success">Ajouter une catégories</a></p>
+</div>
 
-<?php echo var_dump($categories); ?>
 
+<div class="container center" > 
 <table class="tablet table-striped">
     <thead>
         <tr>
@@ -27,7 +49,7 @@ $categories = $select->fetchAll();
             <td><?= $category['name']; ?></td>
             <td>
                 <a href="category_edit.php?id=<?= $category['id']; ?>" class="btn tn-default"> Editer</a>
-                <a href="?delete=<?= $category['id']; ?>" class="btn btn-error" onclick="return confirm('Sur de sur ?');">Supprimer</a>
+                <a href="?delete=<?= $category['id']; ?>&<?= csrf(); ?>" class="btn btn-error" onclick="return confirm('Sur de sur ?');">Supprimer</a>
             </td> 
         </tr>
         
@@ -36,6 +58,7 @@ $categories = $select->fetchAll();
     </tbody>
 
 </table>
+</div>
 
 </div>
 <?php include '../partials/footer.php'; ?>
